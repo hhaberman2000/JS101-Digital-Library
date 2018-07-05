53bec7ffd05c6b649802e9f7bd9b1b5bd774fb92
Hal Haberman <hhaberman@techtonicgroup.com>
2018-07-05 16:55:31 -0600

    commit still need to fix add books modal

--- js/library-script.js
+++ js/library-script.js
@@ -285,6 +285,42 @@ var gBook12= new Book ({
 
 var addBooksArray = [ gBook5, gBook6, gBook7, gBook8, gBook9, gBook10, gBook11, gBook12];
 
+$(document).ready( function() {
+    	$(document).on('change', '.btn-file :file', function() {
+		var input = $(this),
+			label = input.val().replace(/\\/g, '/').replace(/.*\//, '');
+		input.trigger('fileselect', [label]);
+		});
+
+		$('.btn-file :file').on('fileselect', function(event, label) {
+
+		    var input = $(this).parents('.input-group').find(':text'),
+		        log = label;
+
+		    if( input.length ) {
+		        input.val(log);
+		    } else {
+		        if( log ) alert(log);
+		    }
+
+		});
+		function readURL(input) {
+		    if (input.files && input.files[0]) {
+		        var reader = new FileReader();
+
+		        reader.onload = function (e) {
+		            $('#img-upload').attr('src', e.target.result);
+		        }
+
+		        reader.readAsDataURL(input.files[0]);
+		    }
+		}
+
+		$("#imgInp").change(function(){
+		    readURL(this);
+		});
+	});
+
 document.addEventListener("DOMContentLoaded", function() {
   window.gLibrary = new Library();
   window.gLibrary2 = new Library();
